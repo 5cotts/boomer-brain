13c8ef18ae163743ab50020957b3c08b90fd2abc
Scott Schmidt <s@scottschmidt.io>
2022-02-21 22:18:38 -0500

    add enter button and click to change welcome image

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,26 @@
-function randomImgFromArray(arr) {
-    var num = Math.floor( Math.random() * arr.length );
-    var img = arr[ num ];
-    var imgStr = `<img src="${img}" alt="${img}" class="centered-img">`;
-    return imgStr;
+const PATH = "assets/2022_01_15_First_Design";
+const IMAGES = [
+    PATH + "/bloated.png",
+    PATH + "/og_text.png",
+    PATH + "/spotlight.png",
+    PATH + "/20_20_vision.png",
+    PATH + "/swirly.png",
+];
+
+function setImage(imgPath) {
+    document.getElementById("imgClickAndChange").src = imgPath;
+}
+
+function randomImage() {
+    var num = Math.floor( Math.random() * IMAGES.length );
+    var img = IMAGES[ num ];
+    setImage(img);
+};
+
+function changeImage() {
+    let img = IMAGES.shift();
+    IMAGES.push(img)
+    setImage(img);
 };
 
 function contact(contactEmail) {
