dfbe5a9ed48079da2ba0558fee9522d1e6220068
Scott Schmidt <s@scottschmidt.io>
2022-02-27 12:31:05 -0500

    fix contact function

---
+++ scripts/email.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * Initializes the carousel by updating DOM with out own classes.
+ * @param {string} contactEmail - The input email address.
+ * @return {string}
+ */
+function contact(contactEmail) {
+  // TODO: Email to come. Need to set up DNS.
+  const emailStr = `<strike><a href="">${contactEmail}</a></strike>`;
+  return emailStr;
+};
